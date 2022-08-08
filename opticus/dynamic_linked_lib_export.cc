c0873b350ac13ec21fc2976278c66b5e4b505fa0
Noah VERTUEUX <81981323+vertueux@users.noreply.github.com>
2022-08-08 12:17:38 +0200

    I put the C++ sources in the namespace of opticus.
    Mainly to mark the fact that it is a C++ project and not C.

--- opticus/dynamic_linked_lib_export.cc
+++ opticus/dynamic_linked_lib_export.cc
@@ -2,6 +2,10 @@
 
 #include "dynamic_linked_lib_export.h"
 
+namespace opticus {
+
 extern "C" __declspec(dllexport) void __cdecl myprint(const char* str) {
   printf("%s", str);
 }
+
+} // namespace opticus
--- opticus/dynamic_linked_lib_export.h
+++ opticus/dynamic_linked_lib_export.h
@@ -3,4 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+namespace opticus {
+
 extern "C" __declspec(dllexport) void __cdecl myprint(const char* str);
+
+} // namespace opticus
